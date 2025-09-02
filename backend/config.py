cac1949b35ecafb7feedff02bd1ae310de7df86a
olaholunuga <olaholunuga@gmail.com>
2025-09-02 11:27:35 +0100

    setting up the resend api key

--- backend/config.py
+++ backend/config.py
@@ -3,3 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'YOUR_SECRET_KEY_HERE')
     GROQ_API_KEY = os.getenv('GROQ_API_KEY', 'groq-api-key')
+    RESEND_API_KEY = os.getenv('RESEND_API_KEY')
